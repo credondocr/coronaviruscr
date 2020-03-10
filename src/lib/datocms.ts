179ed49efc0a32004a80ebb2cbb1e5307614b89f
Kevin Wolf <hi@kevinwolf.dev>
2020-03-10 15:55:38 -0600

    use content delivery api

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -1,7 +1,46 @@
-import { SiteClient } from 'datocms-client'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import 'isomorphic-unfetch'
+
 import { Case } from '../types/content'
 
-const client = new SiteClient('b5d7d10dacad064cb899ce0d988500')
+const API_URL = 'https://graphql.datocms.com'
+const API_TOKEN = 'b5d7d10dacad064cb899ce0d988500'
 
-export const fetchCases = (): Promise<Case[]> =>
-  client.items.all({ 'filter[type]': 'case' }, { allPages: true })
+const fetchApi = async (query: string, variables: Record<string, any> = {}) => {
+  const json = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  }).then((res) => res.json())
+
+  if (json.errors) {
+    throw new Error('Failed to fetch API')
+  }
+
+  return json.data
+}
+
+const FETCH_CASES_QUERY = `
+{
+  allCases(orderBy: detected_ASC) {
+    id
+    detected
+    updated
+    casestatus
+    gender
+    age
+    nationality
+  }
+}
+`
+
+export const fetchCases = async (): Promise<Case[]> => {
+  const data = await fetchApi(FETCH_CASES_QUERY)
+  return data.allCases
+}

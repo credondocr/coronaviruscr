cc19baaffa55431cd4717bb58ae75762ee10f0fd
asotog <alejandro.soto@rivetlogic.com>
2020-03-13 17:09:12 -0600

    last map updates

--- src/components/cases/map.tsx
+++ src/components/cases/map.tsx
@@ -1,12 +1,18 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React, { useState } from 'react'
-import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
+import ReactMapGL, {
+  NavigationControl,
+  Marker,
+  Layer,
+  Source,
+} from 'react-map-gl'
 
 import * as sdk from '../../lib/sdk'
 import CaseIndicator from './indicator'
 import env from '../../config/env'
 import Card from '../ui/card'
+import colors from '../../config/theme/colors'
 
 type CasesMapProps = {
   cases: sdk.Case[]
@@ -15,6 +21,28 @@ type CasesMapProps = {
 
 const defaultCenter = { lat: 9.9359506, lng: -84.1271644 }
 const defaultZoom = 7
+
+const worldviewFilter = [
+  'all',
+  ['==', ['get', 'iso_3166_1'], 'CR'],
+  [
+    'any',
+    ['==', 'all', ['get', 'worldview']],
+    ['in', 'US', ['get', 'worldview']],
+  ],
+]
+
+const layer = {
+  id: 'adm1join',
+  type: 'fill',
+  source: 'adm1join',
+  'source-layer': 'boundaries_admin_1',
+  filter: worldviewFilter,
+  paint: {
+    'fill-opacity': 0.5,
+    'fill-color': colors.primary,
+  },
+}
 
 const CasesMap: React.FC<CasesMapProps> = ({ cases, sampleData }) => {
   const markers = sampleData || cases
@@ -44,6 +72,13 @@ const CasesMap: React.FC<CasesMapProps> = ({ cases, sampleData }) => {
             <CaseIndicator status={marker.casestatus} />
           </Marker>
         ))}
+        <Source
+          id="adm1join"
+          type="vector"
+          url="mapbox://mapbox.boundaries-adm1-v3"
+        >
+          <Layer {...layer} beforeId="waterway-label" />
+        </Source>
         <NavigationControl sx={{ variant: 'components.map.navigation' }} />
       </ReactMapGL>
     </Card>
